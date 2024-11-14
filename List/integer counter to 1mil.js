7f8e5192b13aab37318be1e87dc2d65864deb61b
Stoicalboar7852 <108503162+Stoicalboar7852@users.noreply.github.com>
2024-11-15 09:22:14 +1000

    Create integer counter to 1mil.js

---
+++ List/integer counter to 1mil.js
@@ -0,0 +1,11 @@
+console.log("program started");
+
+var total = 0;
+var current = 0;
+
+while (current <= 1000000) {
+  total = total + current;
+  current = current + 1
+};﻿
+
+console.log("total = " + total);
